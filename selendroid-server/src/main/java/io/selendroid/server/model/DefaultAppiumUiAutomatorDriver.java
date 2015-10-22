ce6a496876150b8d504bf0d27d6030e2111ccfd3
unknown <anand.sadu@imaginea.com>
2015-10-22 10:30:01 +0530

    addressed the review comments

--- selendroid-server/src/main/java/io/selendroid/server/model/DefaultAppiumUiAutomatorDriver.java
+++ selendroid-server/src/main/java/io/selendroid/server/model/DefaultAppiumUiAutomatorDriver.java
@@ -17,9 +17,11 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.math.BigInteger;
-import java.security.SecureRandom;
-import java.util.*;
+import java.util.List;
+import java.util.Random;
+import java.util.Set;
+import java.util.UUID;
+
 import io.selendroid.server.ServerInstrumentation;
 import io.selendroid.server.android.InstrumentedKeySender;
 import io.selendroid.server.android.KeySender;
@@ -40,6 +42,10 @@ public class DefaultAppiumUiAutomatorDriver implements SelendroidDriver {
     public static final String ACCEPT_SSL_CERTS = "acceptSslCerts";
     public static final String SUPPORTS_NETWORK_CONNECTION = "networkConnectionEnabled";
     public static final String AUTOMATION_NAME = "automationName";
+    public static final String PLATFORM_NAME = "platformName";
+    public static final String PLATFORM_VERSION = "platformVersion";
+    public static final String SERVER_VERSION = "version";
+    public static final String METHOD_NOT_IMPLMENTED_EXCEPTION = "Method has no Implementation";
     private ServerInstrumentation serverInstrumentation = null;
     private KeySender keySender = null;
     private Session session = null;
@@ -51,17 +57,17 @@ public class DefaultAppiumUiAutomatorDriver implements SelendroidDriver {
 
     @Override
     public AndroidElement findElement(By by) {
-        throw new NoSuchMethodImplementationException("Method not implemented");
+        throw new NoSuchMethodImplementationException(METHOD_NOT_IMPLMENTED_EXCEPTION);
     }
 
     @Override
     public List<AndroidElement> findElements(By by) {
-        throw new NoSuchMethodImplementationException("Method not implemented");
+        throw new NoSuchMethodImplementationException(METHOD_NOT_IMPLMENTED_EXCEPTION);
     }
 
     @Override
     public String getCurrentUrl() {
-        throw new NoSuchMethodImplementationException("Method not implemented");
+        throw new NoSuchMethodImplementationException(METHOD_NOT_IMPLMENTED_EXCEPTION);
     }
 
     @Override
@@ -73,28 +79,28 @@ public class DefaultAppiumUiAutomatorDriver implements SelendroidDriver {
     public JSONObject getSessionCapabilities(String sessionId) {
         SelendroidLogger.info("session: " + sessionId);
 
-        JSONObject copy;
+        JSONObject sessionCapabilities;
         try {
-            JSONObject capabilities = session.getCapabilities();
-            if (capabilities != null) {
-                copy = new JSONObject(capabilities.toString());
+            JSONObject defaultSessionCapabilities = session.getCapabilities();
+            if (defaultSessionCapabilities != null) {
+                sessionCapabilities = new JSONObject(defaultSessionCapabilities.toString());
             } else {
-                copy = new JSONObject();
+                sessionCapabilities = new JSONObject();
             }
-            copy.put(TAKES_SCREENSHOT, true);
-            copy.put(BROWSER_NAME, "selendroid");
-            copy.put(AUTOMATION_NAME, "uiautomator");
-            copy.put("platformName", "android");
-            copy.put("platformVersion", serverInstrumentation.getOsVersion());
-            copy.put(ROTATABLE, true);
-            copy.put(PLATFORM, "android");
-            copy.put(SUPPORTS_ALERTS, true);
-            copy.put(SUPPORTS_JAVASCRIPT, true);
-            copy.put(SUPPORTS_NETWORK_CONNECTION, true);
-            copy.put("version", serverInstrumentation.getServerVersion());
-            copy.put(ACCEPT_SSL_CERTS, true);
-            SelendroidLogger.info("capabilities: " + copy);
-            return copy;
+            sessionCapabilities.put(TAKES_SCREENSHOT, true);
+            sessionCapabilities.put(BROWSER_NAME, "selendroid");
+            sessionCapabilities.put(AUTOMATION_NAME, "uiautomator");
+            sessionCapabilities.put(PLATFORM_NAME, "android");
+            sessionCapabilities.put(PLATFORM_VERSION, serverInstrumentation.getOsVersion());
+            sessionCapabilities.put(ROTATABLE, true);
+            sessionCapabilities.put(PLATFORM, "android");
+            sessionCapabilities.put(SUPPORTS_ALERTS, true);
+            sessionCapabilities.put(SUPPORTS_JAVASCRIPT, true);
+            sessionCapabilities.put(SUPPORTS_NETWORK_CONNECTION, true);
+            sessionCapabilities.put(SERVER_VERSION, serverInstrumentation.getServerVersion());
+            sessionCapabilities.put(ACCEPT_SSL_CERTS, true);
+            SelendroidLogger.info("Session capabilities: " + sessionCapabilities);
+            return sessionCapabilities;
         } catch (JSONException e) {
             throw new SelendroidException(e);
         }
@@ -102,178 +108,179 @@ public class DefaultAppiumUiAutomatorDriver implements SelendroidDriver {
 
     @Override
     public JSONObject getFullWindowTree() {
-        throw new NoSuchMethodImplementationException("Method not implemented");
+        throw new NoSuchMethodImplementationException(METHOD_NOT_IMPLMENTED_EXCEPTION);
     }
 
     @Override
     public String getWindowSource() {
-        throw new NoSuchMethodImplementationException("Method not implemented");
+        throw new NoSuchMethodImplementationException(METHOD_NOT_IMPLMENTED_EXCEPTION);
     }
 
     @Override
     public String initializeSession(JSONObject desiredCapabilities) {
-        //Temporary solution for session creation and this needs to be modified.
-        SecureRandom random = new SecureRandom();
-        return new BigInteger(130, random).toString(32);
+        // Temporary solution for session creation and this needs to be modified
+        Random random = new Random();
+        return new UUID(random.nextLong(), random.nextLong()).toString();
     }
 
     @Override
     public void stopSession() {
-        throw new NoSuchMethodImplementationException("Method not implemented");
+        throw new NoSuchMethodImplementationException(METHOD_NOT_IMPLMENTED_EXCEPTION);
     }
 
     @Override
     public void switchContext(String type) {
-        throw new NoSuchMethodImplementationException("Method not implemented");
+        throw new NoSuchMethodImplementationException(METHOD_NOT_IMPLMENTED_EXCEPTION);
     }
 
     @Override
     public byte[] takeScreenshot() {
-        throw new NoSuchMethodImplementationException("Method not implemented");
+        throw new NoSuchMethodImplementationException(METHOD_NOT_IMPLMENTED_EXCEPTION);
     }
 
     @Override
     public Keyboard getKeyboard() {
-        throw new NoSuchMethodImplementationException("Method not implemented");
+        throw new NoSuchMethodImplementationException(METHOD_NOT_IMPLMENTED_EXCEPTION);
     }
 
     @Override
     public String getTitle() {
-        throw new NoSuchMethodImplementationException("Method not implemented");
+        throw new NoSuchMethodImplementationException(METHOD_NOT_IMPLMENTED_EXCEPTION);
     }
 
     @Override
     public void get(String url) {
-        throw new NoSuchMethodImplementationException("Method not implemented");
+        throw new NoSuchMethodImplementationException(METHOD_NOT_IMPLMENTED_EXCEPTION);
     }
 
     @Override
     public TouchScreen getTouch() {
-        throw new NoSuchMethodImplementationException("Method not implemented");
+        throw new NoSuchMethodImplementationException(METHOD_NOT_IMPLMENTED_EXCEPTION);
     }
 
     @Override
     public void addCookie(String url, Cookie cookie) {
-        throw new NoSuchMethodImplementationException("Method not implemented");
+        throw new NoSuchMethodImplementationException(METHOD_NOT_IMPLMENTED_EXCEPTION);
     }
 
     @Override
     public void deleteCookie(String url) {
-        throw new NoSuchMethodImplementationException("Method not implemented");
+        throw new NoSuchMethodImplementationException(METHOD_NOT_IMPLMENTED_EXCEPTION);
     }
 
     @Override
     public void deleteNamedCookie(String url, String name) {
-        throw new NoSuchMethodImplementationException("Method not implemented");
+        throw new NoSuchMethodImplementationException(METHOD_NOT_IMPLMENTED_EXCEPTION);
     }
 
     @Override
     public Set<Cookie> getCookies(String url) {
-        throw new NoSuchMethodImplementationException("Method not implemented");
+        throw new NoSuchMethodImplementationException(METHOD_NOT_IMPLMENTED_EXCEPTION);
     }
 
     @Override
     public Object executeScript(String script, JSONArray args) {
-        throw new NoSuchMethodImplementationException("Method not implemented");
+        throw new NoSuchMethodImplementationException(METHOD_NOT_IMPLMENTED_EXCEPTION);
     }
 
     @Override
     public Object executeScript(String script, Object... args) {
-        throw new NoSuchMethodImplementationException("Method not implemented");
+        throw new NoSuchMethodImplementationException(METHOD_NOT_IMPLMENTED_EXCEPTION);
     }
 
     @Override
     public Object executeAsyncScript(String script, JSONArray args) {
-        throw new NoSuchMethodImplementationException("Method not implemented");
+        throw new NoSuchMethodImplementationException(METHOD_NOT_IMPLMENTED_EXCEPTION);
     }
 
     @Override
     public String getContext() {
-        throw new NoSuchMethodImplementationException("Method not implemented");
+        throw new NoSuchMethodImplementationException(METHOD_NOT_IMPLMENTED_EXCEPTION);
     }
 
     @Override
     public Set<String> getContexts() {
-        throw new NoSuchMethodImplementationException("Method not implemented");
+        throw new NoSuchMethodImplementationException(METHOD_NOT_IMPLMENTED_EXCEPTION);
     }
 
     @Override
     public Dimension getWindowSize() {
-        throw new NoSuchMethodImplementationException("Method not implemented");
+        throw new NoSuchMethodImplementationException(METHOD_NOT_IMPLMENTED_EXCEPTION);
     }
 
     @Override
     public void setFrameContext(Object index) throws JSONException {
-        throw new NoSuchMethodImplementationException("Method not implemented");
+        throw new NoSuchMethodImplementationException(METHOD_NOT_IMPLMENTED_EXCEPTION);
     }
 
     @Override
     public void back() {
-        throw new NoSuchMethodImplementationException("Method not implemented");
+        throw new NoSuchMethodImplementationException(METHOD_NOT_IMPLMENTED_EXCEPTION);
     }
 
     @Override
     public void forward() {
-        throw new NoSuchMethodImplementationException("Method not implemented");
+        throw new NoSuchMethodImplementationException(METHOD_NOT_IMPLMENTED_EXCEPTION);
     }
 
     @Override
     public void refresh() {
-        throw new NoSuchMethodImplementationException("Method not implemented");
+        throw new NoSuchMethodImplementationException(METHOD_NOT_IMPLMENTED_EXCEPTION);
     }
 
     @Override
     public boolean isAlertPresent() {
+        // actual logic that detects alert presence need to be implemented
         return false;
     }
 
     @Override
     public String getAlertText() {
-        throw new NoSuchMethodImplementationException("Method not implemented");
+        throw new NoSuchMethodImplementationException(METHOD_NOT_IMPLMENTED_EXCEPTION);
     }
 
     @Override
     public void setAlertText(CharSequence... keysToSend) {
-        throw new NoSuchMethodImplementationException("Method not implemented");
+        throw new NoSuchMethodImplementationException(METHOD_NOT_IMPLMENTED_EXCEPTION);
     }
 
     @Override
     public void acceptAlert() {
-        throw new NoSuchMethodImplementationException("Method not implemented");
+        throw new NoSuchMethodImplementationException(METHOD_NOT_IMPLMENTED_EXCEPTION);
     }
 
     @Override
     public void dismissAlert() {
-        throw new NoSuchMethodImplementationException("Method not implemented");
+        throw new NoSuchMethodImplementationException(METHOD_NOT_IMPLMENTED_EXCEPTION);
     }
 
     @Override
     public ScreenOrientation getOrientation() {
-        throw new NoSuchMethodImplementationException("Method not implemented");
+        throw new NoSuchMethodImplementationException(METHOD_NOT_IMPLMENTED_EXCEPTION);
     }
 
     @Override
     public void rotate(ScreenOrientation orientation) {
-        throw new NoSuchMethodImplementationException("Method not implemented");
+        throw new NoSuchMethodImplementationException(METHOD_NOT_IMPLMENTED_EXCEPTION);
     }
 
     @Override
     public void setAsyncTimeout(long timeout) {
-        throw new NoSuchMethodImplementationException("Method not implemented");
+        throw new NoSuchMethodImplementationException(METHOD_NOT_IMPLMENTED_EXCEPTION);
     }
 
     @Override
     public void setPageLoadTimeout(long timeout) {
-        throw new NoSuchMethodImplementationException("Method not implemented");
+        throw new NoSuchMethodImplementationException(METHOD_NOT_IMPLMENTED_EXCEPTION);
     }
 
     @Override
     public boolean isAirplaneMode() {
-        throw new NoSuchMethodImplementationException("Method not implemented");
+        throw new NoSuchMethodImplementationException(METHOD_NOT_IMPLMENTED_EXCEPTION);
     }
 
     @Override
     public void roll(int dimensionX, int dimensionY) {
-        throw new NoSuchMethodImplementationException("Method not implemented");
+        throw new NoSuchMethodImplementationException(METHOD_NOT_IMPLMENTED_EXCEPTION);
     }
 }

af15e72865d3ca1c4b2f666df42a8509570f0b62
Sravan Medrapu <sravanmedarapu@gmail.com>
2016-06-21 15:55:10 +0530

    Added spec for driver and updated server version in package.json

---
+++ test/functional/driver-e2e-specs.js
@@ -0,0 +1,69 @@
+import chai from 'chai';
+import chaiAsPromised from 'chai-as-promised';
+import AndroidUiautomator2Driver from '../..';
+import sampleApps from 'sample-apps';
+
+chai.should();
+chai.use(chaiAsPromised);
+
+let driver;
+let defaultCaps = {
+  app: sampleApps('ApiDemos-debug'),
+  deviceName: 'Android',
+  platformName: 'Android'
+};
+
+describe('createSession', function () {
+  before(() => {
+    driver = new AndroidUiautomator2Driver();
+  });
+  afterEach(async () => {
+    await driver.deleteSession();
+  });
+  it('should start android session focusing on default pkg and act', async () => {
+    await driver.createSession(defaultCaps);
+    let {appPackage, appActivity} = await driver.adb.getFocusedPackageAndActivity();
+    appPackage.should.equal('io.appium.android.apis');
+    appActivity.should.equal('.ApiDemos');
+  });
+  it('should start android session focusing on custom pkg and act', async () => {
+    let caps = Object.assign({}, defaultCaps);
+    caps.appPackage = 'io.appium.android.apis';
+    caps.appActivity = '.view.SplitTouchView';
+    await driver.createSession(caps);
+    let {appPackage, appActivity} = await driver.adb.getFocusedPackageAndActivity();
+    appPackage.should.equal(caps.appPackage);
+    appActivity.should.equal(caps.appActivity);
+  });
+  it('should error out for not apk extension', async () => {
+    let caps = Object.assign({}, defaultCaps);
+    caps.app = 'foo';
+    caps.appPackage = 'io.appium.android.apis';
+    caps.appActivity = '.view.SplitTouchView';
+    await driver.createSession(caps).should.eventually.be.rejectedWith(/apk/);
+  });
+  it('should error out for invalid app path', async () => {
+    let caps = Object.assign({}, defaultCaps);
+    caps.app = 'foo.apk';
+    caps.appPackage = 'io.appium.android.apis';
+    caps.appActivity = '.view.SplitTouchView';
+    await driver.createSession(caps).should.eventually.be.rejectedWith(/Could not find/);
+  });
+});
+
+describe('close', function () {
+  before(() => {
+    driver = new AndroidUiautomator2Driver();
+  });
+  afterEach(async () => {
+    await driver.deleteSession();
+  });
+  it('should close application', async () => {
+    await driver.createSession(defaultCaps);
+    await driver.closeApp();
+    let {appPackage} = await driver.adb.getFocusedPackageAndActivity();
+    if (appPackage) {
+      appPackage.should.not.equal("io.appium.android.apis");
+    }
+  });
+});

c5a3daab07f816b33a33187f3748c2a357308beb
Sravan Medrapu <sravanmedarapu@gmail.com>
2016-09-06 21:27:07 +0530

    Downloding UiAutomator2 server apks while installing driver module

---
+++ bin/install.js
@@ -0,0 +1,59 @@
+/* eslint-disable no-console */
+"use strict";
+
+var exec = require("child_process").exec;
+var path = require("path");
+
+var MAX_ATTEMPTS = 50;
+var INTERVAL = 1500;
+var attempts = 1;
+var attemptedToBuild = false;
+
+function doInstall () {
+  var setupUiAutomator2;
+
+  // UiAutomator2 needs Java. Fail early if it doesn't exist
+  var androidHelpers = require('appium-android-driver').androidHelpers;
+  androidHelpers.getJavaVersion().then(function () {
+    var onErr = function (err) {
+      var codeNotBuilt = err.message.indexOf('Cannot find module') !== -1;
+      if (attempts > MAX_ATTEMPTS) {
+        console.log("Tried too many times to install UiAutomator2, failing");
+        console.log("Original error: " + err.message);
+        throw new Error("Unable to import and run the installer. " +
+                        "If you're running from source, run `gulp transpile` " +
+                        "and then re-run `npm install`");
+      }
+      attempts++;
+      if (codeNotBuilt && !attemptedToBuild) {
+        attemptedToBuild = true;
+        console.log("Attempting to transpile setup code...");
+        exec("npm run transpile", {cwd: path.resolve(__dirname, "..")}, function (err) {
+          if (err) {
+            console.warn("Setup code could not be transpiled: " + err.message);
+          } else {
+            console.log("Setup code successfully transpiled");
+          }
+          setTimeout(doInstall, INTERVAL);
+        });
+      } else {
+        console.log("UiAutomator2 setup files did not yet exist, waiting...");
+        setTimeout(doInstall, INTERVAL);
+      }
+    };
+
+    try {
+      setupUiAutomator2 = require('../build/lib/installer').setupUiAutomator2;
+      setupUiAutomator2().catch(onErr);
+    } catch (err) {
+      onErr(err);
+    }
+  }).catch(function () {
+    console.error("Could not find JAVA, skipping UiAutomator2 install.");
+  });
+
+}
+
+if (require.main === module) {
+  doInstall();
+}

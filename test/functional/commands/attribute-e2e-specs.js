3d936964b5f24ab1a2bcc45ecea754c518650f52
Isaac Murchie <isaac@saucelabs.com>
2017-06-27 12:29:33 -0400

    Allow adb port to be changed

--- test/functional/commands/attribute-e2e-specs.js
+++ test/functional/commands/attribute-e2e-specs.js
@@ -38,10 +38,10 @@ describe('apidemo - attributes', function () {
     await driver.getAttribute('contentDescription', animationEl).should.eventually.become("Animation");
   });
   it('should be able to find displayed attribute', async () => {
-    await driver.getAttribute('displayed', animationEl).should.eventually.become(true);
+    await driver.getAttribute('displayed', animationEl).should.eventually.become('true');
   });
   it('should be able to find enabled attribute', async () => {
-    await driver.getAttribute('enabled', animationEl).should.eventually.become(true);
+    await driver.getAttribute('enabled', animationEl).should.eventually.become('true');
   });
   it('should be able to find displayed attribute through normal func', async () => {
     await driver.elementDisplayed(animationEl).should.eventually.become(true);
--- test/functional/driver-e2e-specs.js
+++ test/functional/driver-e2e-specs.js
@@ -2,6 +2,8 @@ import chai from 'chai';
 import chaiAsPromised from 'chai-as-promised';
 import AndroidUiautomator2Driver from '../..';
 import sampleApps from 'sample-apps';
+import ADB from 'appium-adb';
+
 
 chai.should();
 chai.use(chaiAsPromised);
@@ -14,51 +16,88 @@ let defaultCaps = {
 };
 
 describe('createSession', function () {
-  before(() => {
+  before(function () {
     driver = new AndroidUiautomator2Driver();
   });
-  afterEach(async () => {
-    await driver.deleteSession();
-  });
-  it('should start android session focusing on default pkg and act', async () => {
-    await driver.createSession(defaultCaps);
-    let {appPackage, appActivity} = await driver.adb.getFocusedPackageAndActivity();
-    appPackage.should.equal('io.appium.android.apis');
-    appActivity.should.equal('.ApiDemos');
-  });
-  it('should start android session focusing on custom pkg and act', async () => {
-    let caps = Object.assign({}, defaultCaps);
-    caps.appPackage = 'io.appium.android.apis';
-    caps.appActivity = '.view.SplitTouchView';
-    await driver.createSession(caps);
-    let {appPackage, appActivity} = await driver.adb.getFocusedPackageAndActivity();
-    appPackage.should.equal(caps.appPackage);
-    appActivity.should.equal(caps.appActivity);
-  });
-  it('should error out for not apk extension', async () => {
-    let caps = Object.assign({}, defaultCaps);
-    caps.app = 'foo';
-    caps.appPackage = 'io.appium.android.apis';
-    caps.appActivity = '.view.SplitTouchView';
-    await driver.createSession(caps).should.eventually.be.rejectedWith(/apk/);
-  });
-  it('should error out for invalid app path', async () => {
-    let caps = Object.assign({}, defaultCaps);
-    caps.app = 'foo.apk';
-    caps.appPackage = 'io.appium.android.apis';
-    caps.appActivity = '.view.SplitTouchView';
-    await driver.createSession(caps).should.eventually.be.rejectedWith(/Could not find/);
-  });
-  it('should get device model, manufacturer and screen size in session details', async () => {
-    let caps = Object.assign({}, defaultCaps);
-    caps.appPackage = 'io.appium.android.apis';
-    caps.appActivity = '.view.SplitTouchView';
-    await driver.createSession(caps);
 
-    let serverCaps = await driver.getSession();
-    serverCaps.deviceScreenSize.should.exist;
-    serverCaps.deviceModel.should.exist;
-    serverCaps.deviceManufacturer.should.exist;
+  describe('default adb port', function () {
+    afterEach(async function () {
+      await driver.deleteSession();
+    });
+
+    it('should start android session focusing on default pkg and act', async () => {
+      await driver.createSession(defaultCaps);
+      let {appPackage, appActivity} = await driver.adb.getFocusedPackageAndActivity();
+      appPackage.should.equal('io.appium.android.apis');
+      appActivity.should.equal('.ApiDemos');
+    });
+    it('should start android session focusing on custom pkg and act', async () => {
+      let caps = Object.assign({}, defaultCaps, {
+        appPackage: 'io.appium.android.apis',
+        appActivity: '.view.SplitTouchView',
+      });
+      await driver.createSession(caps);
+      let {appPackage, appActivity} = await driver.adb.getFocusedPackageAndActivity();
+      appPackage.should.equal(caps.appPackage);
+      appActivity.should.equal(caps.appActivity);
+    });
+    it('should error out for not apk extension', async () => {
+      let caps = Object.assign({}, defaultCaps, {
+        app: 'foo',
+        appPackage: 'io.appium.android.apis',
+        appActivity: '.view.SplitTouchView',
+      });
+      await driver.createSession(caps).should.eventually.be.rejectedWith(/New app path foo did not have extension \.apk/);
+    });
+    it('should error out for invalid app path', async () => {
+      let caps = Object.assign({}, defaultCaps, {
+        app: 'foo.apk',
+        appPackage: 'io.appium.android.apis',
+        appActivity: '.view.SplitTouchView',
+      });
+      await driver.createSession(caps).should.eventually.be.rejectedWith(/Could not find/);
+    });
+    it('should get device model, manufacturer and screen size in session details', async () => {
+      let caps = Object.assign({}, defaultCaps, {
+        appPackage: 'io.appium.android.apis',
+        appActivity: '.view.SplitTouchView',
+      });
+      await driver.createSession(caps);
+
+      let serverCaps = await driver.getSession();
+      serverCaps.deviceScreenSize.should.exist;
+      serverCaps.deviceModel.should.exist;
+      serverCaps.deviceManufacturer.should.exist;
+    });
+  });
+
+  describe('custom adb port', function () {
+    let adbPort = 5042;
+
+    async function killServer (adbPort) {
+      let adb = await ADB.createADB({adbPort});
+      // await exec(adb.executable.path, adb.executable.defaultArgs.concat('kill-server'));
+      await adb.killServer();
+    }
+
+    before(async function () {
+      await killServer(5037);
+    });
+    afterEach(async function () {
+      await driver.deleteSession();
+
+      await killServer(adbPort);
+    });
+
+    it('should start android session with a custom adb port', async () => {
+      let caps = Object.assign({}, defaultCaps, {
+        adbPort,
+      });
+      await driver.createSession(caps);
+      let {appPackage, appActivity} = await driver.adb.getFocusedPackageAndActivity();
+      appPackage.should.equal('io.appium.android.apis');
+      appActivity.should.equal('.ApiDemos');
+    });
   });
 });
 

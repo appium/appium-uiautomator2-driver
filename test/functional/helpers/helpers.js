2d7fcbc7a8c6d9a690c438eee0796d8ffe189c34
Jonathan Lipps <jlipps@gmail.com>
2018-01-12 09:54:19 -0800

    skip the scrollBackTo test on arm emus since stuff is broken there

--- test/functional/helpers/helpers.js
+++ test/functional/helpers/helpers.js
@@ -1,3 +1,5 @@
+import { exec } from 'teen_process';
+
 async function getLocale (adb) {
   if (await adb.getApiLevel() < 23) {
     return await adb.getDeviceCountry();
@@ -6,4 +8,14 @@ async function getLocale (adb) {
   }
 }
 
-export { getLocale };
+async function isArmEmu () {
+  const archCmd = ['adb', 'shell getprop ro.product.cpu.abi'.split(" ")];
+  const serialCmd = ['adb', ['get-serialno']];
+  const {stdout: arch} = await exec(...archCmd);
+  const {stdout: serial} = await exec(...serialCmd);
+  if (arch.indexOf('arm') !== -1 && serial.indexOf('emulator') === 0) {
+    return true;
+  }
+}
+
+export { getLocale, isArmEmu };

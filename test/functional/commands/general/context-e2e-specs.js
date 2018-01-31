d39b413496e6993ea341256a5261fe25b21057c5
vmaxim <vmaximwork@gmail.com>
2018-01-31 12:17:14 +0300

    Check that the context is interactable.

--- test/functional/commands/general/context-e2e-specs.js
+++ test/functional/commands/general/context-e2e-specs.js
@@ -15,6 +15,8 @@ const caps = _.defaults({
 }, APIDEMOS_CAPS);
 const WEBVIEW = 'WEBVIEW_io.appium.android.apis';
 const NATIVE = 'NATIVE_APP';
+const NATIVE_LOCATOR = "//*[@class='android.widget.TextView']";
+const WEBVIEW_LOCATOR = "//*[text()='This page is a Selenium sandbox']";
 
 describe('apidemo - context', function () {
   let driver;
@@ -40,16 +42,18 @@ describe('apidemo - context', function () {
     let contexts = await driver.contexts();
     await driver.context(contexts[1]);
   });
-  it('should be able to go into native context after restarting app', async function () {
+  it('should be able to go into native context and interact with it after restarting app', async function () {
     await driver.closeApp();
     await driver.launchApp();
     await driver.context(NATIVE);
+    await driver.elementByXPath(NATIVE_LOCATOR);
   });
-  it('should be able to go into native context after resetting app', async function () {
+  it('should be able to go into native context and interact with it after resetting app', async function () {
     await driver.resetApp();
     await driver.context(NATIVE);
+    await driver.elementByXPath(NATIVE_LOCATOR);
   });
-  it('should be able to go into webview context after restarting app', async function () {
+  it('should be able to go into webview context and interact with it after restarting app', async function () {
     // TODO: Fix this on TestObject. Chromedriver does not exist error
     if (process.env.TESTOBJECT_E2E_TESTS) {
       this.skip();
@@ -57,13 +61,15 @@ describe('apidemo - context', function () {
     await driver.closeApp();
     await driver.launchApp();
     await driver.context(WEBVIEW);
+    await driver.elementByXPath(WEBVIEW_LOCATOR);
   });
-  it('should be able to go into webview context after resetting app', async function () {
+  it('should be able to go into webview context and interact with it after resetting app', async function () {
     // TODO: Fix this on TestObject. Chromedriver does not exist error
     if (process.env.TESTOBJECT_E2E_TESTS) {
       this.skip();
     }
     await driver.resetApp();
     await driver.context(WEBVIEW);
+    await driver.elementByXPath(WEBVIEW_LOCATOR);
   });
 });

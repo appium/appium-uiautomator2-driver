33a0a5334eabf3574ccae306a3dd90b9756daa32
Isaac Murchie <isaac@saucelabs.com>
2018-02-12 11:40:19 -0500

    Spelling

--- test/functional/commands/find/by-accessibility-id-e2e-specs.js
+++ test/functional/commands/find/by-accessibility-id-e2e-specs.js
@@ -16,7 +16,7 @@ describe('Find - accessibility ID', function () {
     await driver.quit();
   });
   it('should find an element by name', async function () {
-    console.log(await driver.souce()); // eslint-disable-line
+    console.log(await driver.source()); // eslint-disable-line
     await driver.elementByAccessibilityId('Animation').should.eventually.exist;
   });
   it('should return an array of one element if the `multi` param is true', async function () {

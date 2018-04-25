5082cffb65e3a440bbd558f524fa6094d6432525
dan@saucelabs.com <dan@saucelabs.com>
2018-04-25 14:35:08 -0700

    Fix

--- run-sauce.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-concurrently \
-"export PLATFORM_VERSION=7.0 && export DEVICE_NAME='Samsung Galaxy S6 GoogleAPI Emulator' && mocha -t 7000000 build/test/functional/commands/find/" \ 
-"echo FUCK!!!!" \ 
-"export PLATFORM_VERSION=7.0 && export DEVICE_NAME='Samsung Galaxy S6 GoogleAPI Emulator' && mocha -t 7000000 build/test/functional/commands/general/" \

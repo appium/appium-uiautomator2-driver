afbf757eb7c683b8006829c978c5f6c2f73f18af
Isaac A. Murchie <isaac.appium@gmail.com>
2018-05-18 10:18:51 -0400

    Update apidemos app (#169)

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,6 @@ const boilerplate = require('appium-gulp-plugins').boilerplate.use(gulp);
 
 boilerplate({
   build: 'appium-uiautomator2-driver',
-  jscs: false,
   testTimeout: 120000,
   e2eTest: { android: true }
 });

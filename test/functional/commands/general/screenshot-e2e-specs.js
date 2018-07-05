c9a8f67c1ceabd5d2ed631c6094c2febffdb322d
Jonathan Lipps <jlipps@gmail.com>
2018-07-04 23:26:54 -0700

    support mjpeg stream screenshot via new mjpegScreenshotUrl cap

---
+++ test/functional/commands/general/screenshot-e2e-specs.js
@@ -0,0 +1,31 @@
+import chai from 'chai';
+import chaiAsPromised from 'chai-as-promised';
+import { mjpeg } from 'appium-support';
+import { APIDEMOS_CAPS } from '../../desired';
+import { initDriver } from '../../helpers/session';
+
+
+chai.should();
+chai.use(chaiAsPromised);
+
+const MJPEG_SERVER_PORT = 8589;
+const MJPEG_SERVER_URL = `http://localhost:${MJPEG_SERVER_PORT}`;
+
+describe('screenshot - mjpeg server', function () {
+  let driver, mjpegServer;
+
+  before(async function () {
+    mjpegServer = mjpeg.initMJpegServer(MJPEG_SERVER_PORT);
+    const caps = {...APIDEMOS_CAPS, mjpegScreenshotUrl: MJPEG_SERVER_URL};
+    driver = await initDriver(caps);
+  });
+  after(async function () {
+    await driver.quit();
+    mjpegServer.close();
+  });
+  it('should get the screenshot via an mjpeg server if requested', async function () {
+    const img = await driver.takeScreenshot();
+    img.indexOf('iVBOR').should.eql(0);
+    img.length.should.be.above(400);
+  });
+});

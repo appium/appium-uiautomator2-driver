efad692babc0851ee44813f5b934f84c304e7ed7
Isaac Murchie <isaac.appium@gmail.com>
2019-04-12 11:50:49 -0400

    Fix Travis build

--- test/functional/helpers/session.js
+++ test/functional/helpers/session.js
@@ -2,13 +2,16 @@ import ADB from 'appium-adb';
 import { DEFAULT_HOST, DEFAULT_PORT } from '../../..';
 import logger from '../../../lib/logger';
 import wd from 'wd';
-import { retry } from 'asyncbox';
+import { retry, retryInterval } from 'asyncbox';
+import _ from 'lodash';
 import './mocha-scripts';
 
 
 const {SAUCE_RDC, SAUCE_EMUSIM, CLOUD, TRAVIS, CI} = process.env;
 
 const INIT_RETRIES = process.env.CI ? 2 : 1;
+const ALERT_CHECK_RETRIES = 5;
+const ALERT_CHECK_INTERVAL = 1000;
 
 function getPort () {
   if (SAUCE_EMUSIM || SAUCE_RDC) {
@@ -56,16 +59,36 @@ async function initSession (caps, adbPort) {
 
   // In Travis, there is sometimes a popup
   if (CI && !CLOUD) {
-    try {
-      const okBtn = await driver.elementById('android:id/button1');
-      logger.warn('*******************************************************');
-      logger.warn('*******************************************************');
-      logger.warn('*******************************************************');
-      logger.warn('Alert found on session startup. Trying to dismiss alert');
-      await okBtn.click();
-      await driver.startActivity(caps);
-    } catch (ign) {}
+    const startArgs = _.pick(caps, ['appPackage', 'appActivity', 'appWaitPackage', 'appWaitActivity', 'intentAction', 'intentCategory', 'intentFlags', 'optionalIntentArguments', 'dontStopAppOnReset', 'sessionId', 'id']);
+    for (const btnId of ['android:id/button1', 'android:id/aerr_wait']) {
+      let alertFound = false;
+      await retryInterval(ALERT_CHECK_RETRIES, ALERT_CHECK_INTERVAL, async function () {
+        let btn;
+        try {
+          btn = await driver.elementById(btnId);
+          alertFound = true;
+        } catch (ign) {
+          // no element found, so just finish
+          return;
+        }
+        logger.warn('*******************************************************');
+        logger.warn('*******************************************************');
+        logger.warn('*******************************************************');
+        logger.warn('Alert found on session startup. Trying to dismiss alert');
+        logger.warn('*******************************************************');
+        logger.warn('*******************************************************');
+        logger.warn('*******************************************************');
+        await btn.click();
+        throw new Error('Alert was found, retry');
+      });
+      // if an alert was ever found, try to start the activity that the session should have started
+      if (alertFound) {
+        await driver.startActivity(startArgs);
+      }
+    }
   }
+
+  await driver.setImplicitWaitTimeout(process.env.CI ? 30000 : 5000);
 
   return driver;
 }

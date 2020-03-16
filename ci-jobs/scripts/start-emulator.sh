be1b2c698cbecdf1e3e423da8176ea5970204f9d
Isaac A. Murchie <isaac.appium@gmail.com>
2020-03-16 16:25:47 -0400

    build: use acceleration in azure emus (#375)
    * build: use acceleration in azure emus
    
    * add little sleep

--- ci-jobs/scripts/start-emulator.sh
+++ ci-jobs/scripts/start-emulator.sh
@@ -19,9 +19,11 @@ echo $ANDROID_HOME/emulator/emulator -list-avds
 echo "Starting emulator"
 
 # Start emulator in background
-nohup $ANDROID_HOME/emulator/emulator -avd testemulator -no-snapshot > /dev/null 2>&1 &
+nohup $ANDROID_HOME/emulator/emulator -avd testemulator -accel auto -nojni -no-boot-anim -no-snapshot > /dev/null 2>&1 &
 $ANDROID_HOME/platform-tools/adb wait-for-device shell 'while [[ -z $(getprop sys.boot_completed | tr -d '\r') ]]; do sleep 1; done; input keyevent 82'
 
 $ANDROID_HOME/platform-tools/adb devices
+
+sleep 30s
 
 echo "Emulator started"

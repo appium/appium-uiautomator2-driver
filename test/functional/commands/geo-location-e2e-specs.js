4e89aab4d258969e2b8b3c3ac61cdd318fb4e968
Dan Graham <dpgraham.85@gmail.com>
2020-05-04 00:34:45 -0700

    chore: Add SDK 29 to validation (#390)

--- test/functional/commands/geo-location-e2e-specs.js
+++ test/functional/commands/geo-location-e2e-specs.js
@@ -3,13 +3,13 @@ import chaiAsPromised from 'chai-as-promised';
 import { retryInterval } from 'asyncbox';
 import { GPS_DEMO_CAPS } from '../desired';
 import { initSession, deleteSession } from '../helpers/session';
+import B from 'bluebird';
 
 
 chai.should();
 chai.use(chaiAsPromised);
 
 describe('geo-location -', function () {
-  // TODO: Skip for SDK 28+
   let driver;
   beforeEach(async function () {
     driver = await initSession(GPS_DEMO_CAPS);
@@ -23,6 +23,20 @@ describe('geo-location -', function () {
   }
 
   it('should set geo location', async function () {
+    // If we hit the permission screen, click the 'Continue Button' (sdk >= 28)
+    const continueButtons = await driver.elementsById('com.android.permissioncontroller:id/continue_button');
+    if (continueButtons.length > 0) {
+      await continueButtons[0].click();
+    }
+
+    // Get rid of the modal window saying that the app was built for an old version
+    await B.delay(1000);
+    const okButtons = await driver.elementsById('android:id/button1');
+    if (okButtons.length > 0) {
+      await okButtons[0].click();
+    }
+
+    // Get the text in the app that tells us the latitude and logitude
     const getText = async function () {
       return await retryInterval(10, 1000, async function () {
         const textViews = await driver.elementsByClassName('android.widget.TextView');

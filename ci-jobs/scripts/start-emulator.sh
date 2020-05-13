e771d8395d884795511aafa49d327a179102cba2
Mykola Mokhnach <mokhnach@gmail.com>
2020-05-13 10:48:37 +0200

    fix: Use the default emulator config for CI tests on Azure (#395)

--- ci-jobs/scripts/start-emulator.sh
+++ ci-jobs/scripts/start-emulator.sh
@@ -8,7 +8,7 @@
 export JAVA_HOME=`/usr/libexec/java_home -v 1.8`
 
 # Install AVD files
-declare -r emulator="system-images;android-$ANDROID_SDK_VERSION;google_apis;x86"
+declare -r emulator="system-images;android-$ANDROID_SDK_VERSION;default;x86"
 echo "y" | $ANDROID_HOME/tools/bin/sdkmanager --install "$emulator"
 
 # Create emulator
@@ -20,10 +20,31 @@ echo "Starting emulator"
 
 # Start emulator in background
 nohup $ANDROID_HOME/emulator/emulator -avd testemulator -accel auto -nojni -no-boot-anim -no-snapshot > /dev/null 2>&1 &
-$ANDROID_HOME/platform-tools/adb wait-for-device shell 'while [[ -z $(getprop sys.boot_completed | tr -d '\r') ]]; do sleep 1; done; input keyevent 82'
+
+$ANDROID_HOME/platform-tools/adb wait-for-device get-serialno
+secondsStarted=`date +%s`
+TIMEOUT=360
+while [[ $(( `date +%s` - $secondsStarted )) -lt $TIMEOUT ]]; do
+  # Fail fast if Emulator process crashed
+  pgrep -nf avd || exit 1
+
+  processList=`adb shell ps`
+  if [[ "$processList" =~ "com.android.systemui" ]]; then
+    echo "System UI process is running. Checking IME services availability"
+    $ANDROID_HOME/platform-tools/adb shell ime list && break
+  fi
+  sleep 5
+  secondsElapsed=$(( `date +%s` - $secondsStarted ))
+  secondsLeft=$(( $TIMEOUT - $secondsElapsed ))
+  echo "Waiting until emulator finishes services startup; ${secondsElapsed}s elapsed; ${secondsLeft}s left"
+done
+
+bootDuration=$(( `date +%s` - $secondsStarted ))
+if [[ $bootDuration -ge $TIMEOUT ]]; then
+  echo "Emulator has failed to fully start within ${TIMEOUT}s"
+  exit 1
+fi
+echo "Emulator booting took ${bootDuration}s"
+adb shell input keyevent 82
 
 $ANDROID_HOME/platform-tools/adb devices
-
-sleep 30s
-
-echo "Emulator started"

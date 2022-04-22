c1f81e90d63325c48a126318d0b6f088fb82fa1b
Mykola Mokhnach <mokhnach@gmail.com>
2022-04-22 20:34:07 +0200

    refactor: Update some e2e tests for webdriverio compatibility (#514)

--- test/functional/desired.js
+++ test/functional/desired.js
@@ -1,59 +1,69 @@
 import _ from 'lodash';
-import '../env';
-
 
 const uiautomator2ServerLaunchTimeout = (process.env.TRAVIS || process.env.CI) ? 60000 : 20000;
 const uiautomator2ServerInstallTimeout = (process.env.TRAVIS || process.env.CI) ? 120000 : 20000;
 
 const ADB_EXEC_TIMEOUT = (process.env.TRAVIS || process.env.CI) ? 60000 : 20000;
 
-const GENERIC_CAPS = {
-  deviceName: 'Android',
-  platformName: 'Android',
-  uiautomator2ServerLaunchTimeout,
-  uiautomator2ServerInstallTimeout,
-  automationName: 'uiautomator2',
-  adbExecTimeout: ADB_EXEC_TIMEOUT,
-  ignoreHiddenApiPolicyError: true,
-};
-
-if (process.env.CLOUD) {
-  GENERIC_CAPS.platformVersion = process.env.CLOUD_PLATFORM_VERSION;
-  GENERIC_CAPS.deviceName = process.env.CLOUD_DEVICE_NAME;
-  GENERIC_CAPS.build = process.env.SAUCE_BUILD;
-  GENERIC_CAPS[process.env.APPIUM_BUNDLE_CAP] = {'appium-url': 'sauce-storage:appium.zip'};
+function deepFreeze (object) {
+  const propNames = Object.getOwnPropertyNames(object);
+  for (const name of propNames) {
+    const value = object[name];
+    if (value && typeof value === 'object') {
+      deepFreeze(value);
+    }
+  }
+  return Object.freeze(object);
 }
 
+function amendCapabilities (baseCaps, ...newCaps) {
+  return deepFreeze({
+    alwaysMatch: _.cloneDeep(Object.assign({}, baseCaps.alwaysMatch, ...newCaps)),
+    firstMatch: [{}],
+  });
+}
+
+const GENERIC_CAPS = deepFreeze({
+  firstMatch: [{}],
+  alwaysMatch: {
+    'appium:deviceName': 'Android',
+    platformName: 'Android',
+    'appium:uiautomator2ServerLaunchTimeout': uiautomator2ServerLaunchTimeout,
+    'appium:uiautomator2ServerInstallTimeout': uiautomator2ServerInstallTimeout,
+    'appium:automationName': 'uiautomator2',
+    'appium:adbExecTimeout': ADB_EXEC_TIMEOUT,
+    'appium:ignoreHiddenApiPolicyError': true,
+  }
+});
 
 
-const apiDemosApp = process.env.CLOUD
-  ? 'http://appium.github.io/appium/assets/ApiDemos-debug.apk'
-  : require.resolve('android-apidemos');
-const gpsDemoApp = process.env.CLOUD
-  ? 'http://appium.github.io/appium/assets/gpsDemo-debug.apk'
-  : require('gps-demo-app');
+const apiDemosApp = require.resolve('android-apidemos');
+const gpsDemoApp = require('gps-demo-app');
 
-const APIDEMOS_CAPS = _.defaults({
-  app: apiDemosApp,
-  appPackage: 'io.appium.android.apis',
-  appActivity: '.ApiDemos',
-  disableWindowAnimation: true,
-}, GENERIC_CAPS);
+const APIDEMOS_CAPS = amendCapabilities(GENERIC_CAPS, {
+  'appium:app': apiDemosApp,
+  'appium:appPackage': 'io.appium.android.apis',
+  'appium:appActivity': '.ApiDemos',
+  'appium:disableWindowAnimation': true,
+});
 
-const SCROLL_CAPS = _.defaults({
-  app: apiDemosApp,
-  appPackage: 'io.appium.android.apis',
-  appActivity: '.view.ScrollView2',
-}, GENERIC_CAPS);
+const SCROLL_CAPS = amendCapabilities(GENERIC_CAPS, {
+  'appium:app': apiDemosApp,
+  'appium:appPackage': 'io.appium.android.apis',
+  'appium:appActivity': '.view.ScrollView2',
+});
 
-const GPS_DEMO_CAPS = _.defaults({
-  app: gpsDemoApp,
-  appPackage: 'de.impressive.artworx.tutorials.gps',
-  appActivity: '.GPSTest',
-}, GENERIC_CAPS);
+const GPS_DEMO_CAPS = amendCapabilities(GENERIC_CAPS, {
+  'appium:app': gpsDemoApp,
+  'appium:appPackage': 'de.impressive.artworx.tutorials.gps',
+  'appium:appActivity': '.GPSTest',
+});
 
-const BROWSER_CAPS = _.defaults({
+const BROWSER_CAPS = amendCapabilities(GENERIC_CAPS, {
   browserName: 'Chrome',
-}, GENERIC_CAPS);
+});
 
-export { GENERIC_CAPS, APIDEMOS_CAPS, GPS_DEMO_CAPS, BROWSER_CAPS, SCROLL_CAPS };
+export {
+  GENERIC_CAPS, APIDEMOS_CAPS, GPS_DEMO_CAPS, BROWSER_CAPS, SCROLL_CAPS,
+  amendCapabilities
+};
--- test/functional/driver-e2e-specs.js
+++ test/functional/driver-e2e-specs.js
@@ -1,15 +1,13 @@
 import chai from 'chai';
 import chaiAsPromised from 'chai-as-promised';
 import ADB from 'appium-adb';
-import axios from 'axios';
-import { DEFAULT_HOST, DEFAULT_PORT } from '../..';
-import { APIDEMOS_CAPS } from './desired';
+import { APIDEMOS_CAPS, amendCapabilities } from './desired';
 import { initSession, deleteSession } from './helpers/session';
 import B from 'bluebird';
 import { retryInterval } from 'asyncbox';
 
 
-const should = chai.should();
+chai.should();
 chai.use(chaiAsPromised);
 
 const APIDEMOS_PACKAGE = 'io.appium.android.apis';
@@ -19,20 +17,19 @@ const APIDEMOS_SPLIT_TOUCH_ACTIVITY = '.view.SplitTouchView';
 const DEFAULT_ADB_PORT = 5037;
 
 async function killAndPrepareServer (oldPort, newPort) {
-  if (!process.env.TESTOBJECT_E2E_TESTS) {
-    let adb = await ADB.createADB({adbPort: oldPort});
-    await adb.killServer();
-    if (process.env.CI) {
-      // on Travis this takes a while to get into a good state
-      await B.delay(10000);
-    }
-    adb = await ADB.createADB({adbPort: newPort});
-    await retryInterval(5, 500, async () => await adb.getApiLevel());
+  const oldAdb = await ADB.createADB({adbPort: oldPort});
+  await oldAdb.killServer();
+  if (process.env.CI) {
+    // on Travis this takes a while to get into a good state
+    await B.delay(10000);
   }
+  const newAdb = await ADB.createADB({adbPort: newPort});
+  await retryInterval(5, 500, async () => await newAdb.getApiLevel());
 }
 
 describe('createSession', function () {
   let driver;
+
   describe('default adb port', function () {
     afterEach(async function () {
       if (driver) {
@@ -43,79 +40,37 @@ describe('createSession', function () {
 
     it('should start android session focusing on default pkg and act', async function () {
       driver = await initSession(APIDEMOS_CAPS);
-      let appPackage = await driver.getCurrentPackage();
-      let appActivity = await driver.getCurrentDeviceActivity();
-      appPackage.should.equal(APIDEMOS_PACKAGE);
-      appActivity.should.equal(APIDEMOS_MAIN_ACTIVITY);
+      await driver.getCurrentPackage().should.eventually.equal(APIDEMOS_PACKAGE);
+      await driver.getCurrentActivity().should.eventually.equal(APIDEMOS_MAIN_ACTIVITY);
     });
     it('should start android session focusing on custom pkg and act', async function () {
-      let caps = Object.assign({}, APIDEMOS_CAPS, {
-        appPackage: APIDEMOS_PACKAGE,
-        appActivity: APIDEMOS_SPLIT_TOUCH_ACTIVITY,
+      const caps = amendCapabilities(APIDEMOS_CAPS, {
+        'appium:appPackage': APIDEMOS_PACKAGE,
+        'appium:appActivity': APIDEMOS_SPLIT_TOUCH_ACTIVITY,
       });
       driver = await initSession(caps);
-      let appPackage = await driver.getCurrentPackage();
-      let appActivity = await driver.getCurrentDeviceActivity();
-      appPackage.should.equal(caps.appPackage);
-      appActivity.should.equal(caps.appActivity);
+      await driver.getCurrentPackage().should.eventually.equal(APIDEMOS_PACKAGE);
+      await driver.getCurrentActivity().should.eventually.equal(APIDEMOS_SPLIT_TOUCH_ACTIVITY);
     });
     it('should error out for not apk extension', async function () {
-      // Don't test this on TestObject. The 'app' cap gets stripped out and can't be tested
-      if (process.env.TESTOBJECT_E2E_TESTS) {
-        return;
-      }
-      let caps = Object.assign({}, APIDEMOS_CAPS, {
-        app: 'foo',
-        appPackage: APIDEMOS_PACKAGE,
-        appActivity: APIDEMOS_SPLIT_TOUCH_ACTIVITY,
+      const caps = amendCapabilities(APIDEMOS_CAPS, {
+        'appium:app': 'foo',
+        'appium:appPackage': APIDEMOS_PACKAGE,
+        'appium:appActivity': APIDEMOS_SPLIT_TOUCH_ACTIVITY,
       });
-      try {
-        await initSession(caps);
-        throw new Error(`Call to 'initSession' should not have succeeded`);
-      } catch (e) {
-        e.data.should.match(/does not exist or is not accessible/);
-      }
+      await initSession(caps).should.eventually.be.rejectedWith(/does not exist or is not accessible/);
     });
     it('should error out for invalid app path', async function () {
-      // Don't test this on TestObject. The 'app' cap gets stripped out and can't be tested
-      if (process.env.TESTOBJECT_E2E_TESTS) {
-        return;
-      }
-      let caps = Object.assign({}, APIDEMOS_CAPS, {
-        app: 'foo.apk',
-        appPackage: APIDEMOS_PACKAGE,
-        appActivity: APIDEMOS_SPLIT_TOUCH_ACTIVITY,
+      const caps = amendCapabilities(APIDEMOS_CAPS, {
+        'appium:app': 'foo.apk',
+        'appium:appPackage': APIDEMOS_PACKAGE,
+        'appium:appActivity': APIDEMOS_SPLIT_TOUCH_ACTIVITY,
       });
-
-      try {
-        await initSession(caps);
-        throw new Error(`Call to 'initSession' should not have succeeded`);
-      } catch (e) {
-        e.data.should.match(/does not exist or is not accessible/);
-      }
-    });
-    it('should get device model, manufacturer and screen size in session details', async function () {
-      let caps = Object.assign({}, APIDEMOS_CAPS, {
-        appPackage: APIDEMOS_PACKAGE,
-        appActivity: APIDEMOS_SPLIT_TOUCH_ACTIVITY,
-      });
-      driver = await initSession(caps);
-
-      let serverCaps = await driver.sessionCapabilities();
-      serverCaps.deviceScreenSize.should.exist;
-      serverCaps.deviceScreenDensity.should.exist;
-      serverCaps.deviceModel.should.exist;
-      serverCaps.deviceManufacturer.should.exist;
-      serverCaps.deviceApiLevel.should.be.greaterThan(0);
+      await initSession(caps).should.eventually.be.rejectedWith(/does not exist or is not accessible/);
     });
   });
 
   describe('custom adb port', function () {
-    // Don't do these tests on TestObject. Cannot use TestObject's ADB.
-    if (process.env.TESTOBJECT_E2E_TESTS) {
-      return;
-    }
-
     let adbPort = 5042;
     let driver;
 
@@ -126,52 +81,27 @@ describe('createSession', function () {
       if (driver) {
         await deleteSession();
       }
+      driver = null;
 
       await killAndPrepareServer(adbPort, DEFAULT_ADB_PORT);
     });
 
     it('should start android session with a custom adb port', async function () {
-      const caps = Object.assign({}, APIDEMOS_CAPS, {
-        adbPort,
-        allowOfflineDevices: true,
+      const caps = amendCapabilities(APIDEMOS_CAPS, {
+        'appium:adbPort': adbPort,
+        'appium:allowOfflineDevices': true,
       });
       driver = await initSession(caps, adbPort);
-      const appPackage = await driver.getCurrentPackage();
-      const appActivity = await driver.getCurrentDeviceActivity();
-      appPackage.should.equal(APIDEMOS_PACKAGE);
-      appActivity.should.equal(APIDEMOS_MAIN_ACTIVITY);
-    });
-  });
-
-  describe('w3c compliance', function () {
-    it('should start a session with W3C caps', async function () {
-      const { value, sessionId, status } = (await axios({
-        url: `http://${DEFAULT_HOST}:${DEFAULT_PORT}/session`,
-        method: 'POST',
-        data: {
-          capabilities: {
-            alwaysMatch: APIDEMOS_CAPS,
-            firstMatch: [{}],
-          }
-        }
-      })).data;
-      value.should.exist;
-      value.capabilities.should.exist;
-      value.sessionId.should.exist;
-      should.not.exist(sessionId);
-      should.not.exist(status);
-      await axios.delete(`http://${DEFAULT_HOST}:${DEFAULT_PORT}/session/${value.sessionId}`);
+      await driver.getCurrentPackage().should.eventually.equal(APIDEMOS_PACKAGE);
+      await driver.getCurrentActivity().should.eventually.equal(APIDEMOS_MAIN_ACTIVITY);
     });
   });
 });
 
 describe('close', function () {
   it('should close application', async function () {
-    let driver = await initSession(APIDEMOS_CAPS);
+    const driver = await initSession(APIDEMOS_CAPS);
     await driver.closeApp();
-    let appPackage = await driver.getCurrentPackage();
-    if (appPackage) {
-      appPackage.should.not.equal(APIDEMOS_PACKAGE);
-    }
+    APIDEMOS_PACKAGE.should.not.equal(await driver.getCurrentPackage());
   });
 });

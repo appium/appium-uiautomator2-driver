4fd88ac6313a26bd46e65675f295da6c2d295439
Mykola Mokhnach <mokhnach@gmail.com>
2022-09-01 21:06:31 +0200

    ci: Switch to GitHub Actions (#539)

--- test/functional/desired.js
+++ test/functional/desired.js
@@ -1,10 +1,10 @@
 import _ from 'lodash';
 import path from 'path';
 
-const uiautomator2ServerLaunchTimeout = (process.env.TRAVIS || process.env.CI) ? 60000 : 20000;
-const uiautomator2ServerInstallTimeout = (process.env.TRAVIS || process.env.CI) ? 120000 : 20000;
+const uiautomator2ServerLaunchTimeout = process.env.CI ? 60000 : 20000;
+const uiautomator2ServerInstallTimeout = process.env.CI ? 120000 : 20000;
 
-const ADB_EXEC_TIMEOUT = (process.env.TRAVIS || process.env.CI) ? 60000 : 20000;
+const ADB_EXEC_TIMEOUT = process.env.CI ? 60000 : 20000;
 
 function deepFreeze (object) {
   const propNames = Object.getOwnPropertyNames(object);

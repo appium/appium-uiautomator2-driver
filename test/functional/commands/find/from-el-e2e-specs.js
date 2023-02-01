33039f9ec0e00f066f78ba88f7de1f6edd88c9ae
rerorero <nato9598@hotmail.co.jp>
2023-02-01 21:57:56 +0900

    ci: fix from-el-e2e-specs

--- test/functional/commands/find/from-el-e2e-specs.js
+++ test/functional/commands/find/from-el-e2e-specs.js
@@ -7,10 +7,9 @@ import { initSession, deleteSession } from '../../helpers/session';
 chai.should();
 chai.use(chaiAsPromised);
 
-const atv = 'android.widget.TextView';
-const alv = 'android.widget.ListView';
-
 describe('Find - from element', function () {
+  const atv = 'android.widget.TextView';
+  const alv = 'android.widget.ListView';
   let driver;
   before(async function () {
     driver = await initSession(APIDEMOS_CAPS);
@@ -19,13 +18,13 @@ describe('Find - from element', function () {
     await deleteSession();
   });
   it('should find a single element by tag name', async function () {
-    let el = await driver.elementByClassName(alv);
-    let innerEl = await el.elementByClassName(atv);
-    await innerEl.text().should.eventually.equal("Access'ibility");
+    const el = await driver.$(alv);
+    const innerEl = await el.$(atv);
+    await innerEl.getText().should.eventually.equal("Access'ibility");
   });
   it('should find multiple elements by tag name', async function () {
-    let el = await driver.elementByClassName(alv);
-    let innerEls = await el.elementsByClassName(atv);
-    await innerEls.should.have.length.above(1);
+    const el = await driver.$(alv);
+    const innerEls = await el.$$(atv);
+    innerEls.should.have.length.above(1);
   });
 });

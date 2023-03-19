a366b046b34c1008c6abac931612c8d111399ac0
rerorero <nato9598@hotmail.co.jp>
2023-03-19 10:31:32 +0900

    ci: remove unnecessary noReset

--- test/functional/desired.js
+++ test/functional/desired.js
@@ -63,7 +63,6 @@ const GPS_DEMO_CAPS = amendCapabilities(GENERIC_CAPS, {
 
 const BROWSER_CAPS = amendCapabilities(GENERIC_CAPS, {
   browserName: 'Chrome',
-  'appium:noReset': true,
 });
 
 const SETTINGS_CAPS = amendCapabilities(GENERIC_CAPS, {

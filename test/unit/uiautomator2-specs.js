7c30588fa941d8b349a3a660a62c86f21021d89b
Kazuaki Matsuo <fly.49.89.over@gmail.com>
2024-06-20 23:32:28 -0700

    extract some condition check and add tests

--- test/unit/uiautomator2-specs.js
+++ test/unit/uiautomator2-specs.js
@@ -13,6 +13,182 @@ chai.use(chaiAsPromised);
 
 describe('UiAutomator2', function () {
   const adb = new ADB();
+
+  describe('shouldUninstallServerPackages', function () {
+    let uiautomator2;
+    const serverApk = {
+      'appPath': 'path/to/appium-uiautomator2-server.apk',
+      'appId': 'io.appium.uiautomator2.server'
+    };
+    const serverTestApk = {
+      'appPath': 'path/to/appium-uiautomator2-server-test.apk',
+      'appId': 'io.appium.uiautomator2.server.test'
+    };
+    beforeEach(function () {
+      uiautomator2 = new UiAutomator2Server(log, {
+        adb,
+        tmpDir: 'tmp',
+        systemPort: 4724,
+        host: 'localhost',
+        devicePort: 6790,
+        disableWindowAnimation: false,
+        disableSuppressAccessibilityService: false
+      });
+    });
+    it('with newer servers are installed', function () {
+      uiautomator2.shouldUninstallServerPackages([
+        {
+          'installState': adb.APP_INSTALL_STATE.NEWER_VERSION_INSTALLED,
+          ...serverApk
+        },
+        {
+          'installState': adb.APP_INSTALL_STATE.NEWER_VERSION_INSTALLED,
+          ...serverTestApk
+        }
+      ]).should.eql(true);
+    }),
+    it('with newer server is installed but the other could be old one', function () {
+      // Then, enforce to uninstall all apks
+      uiautomator2.shouldUninstallServerPackages([
+        {
+          'installState': adb.APP_INSTALL_STATE.NEWER_VERSION_INSTALLED,
+          ...serverApk
+        },
+        {
+          'installState': adb.APP_INSTALL_STATE.OLDER_VERSION_INSTALLED,
+          ...serverTestApk
+        }
+      ]).should.eql(true);
+    }),
+    it('with newer server is installed', function () {
+      uiautomator2.shouldUninstallServerPackages([
+        {
+          'installState': adb.APP_INSTALL_STATE.SAME_VERSION_INSTALLED,
+          ...serverApk
+        },
+        {
+          'installState': adb.APP_INSTALL_STATE.SAME_VERSION_INSTALLED,
+          ...serverTestApk
+        }
+      ]).should.eql(false);
+    }),
+    it('with older servers are installed', function () {
+      // then, installing newer serves are sufficient.
+      uiautomator2.shouldUninstallServerPackages([
+        {
+          'installState': adb.APP_INSTALL_STATE.OLDER_VERSION_INSTALLED,
+          ...serverApk
+        },
+        {
+          'installState': adb.APP_INSTALL_STATE.OLDER_VERSION_INSTALLED,
+          ...serverTestApk
+        }
+      ]).should.eql(false);
+    }),
+    it('with no server are installed', function () {
+      uiautomator2.shouldUninstallServerPackages([
+        {
+          'installState': adb.APP_INSTALL_STATE.NOT_INSTALLED,
+          'appPath': 'path/to/appium-uiautomator2-server.apk',
+          'appId': 'io.appium.uiautomator2.server'
+        },
+        {
+          'installState': adb.APP_INSTALL_STATE.NOT_INSTALLED,
+          'appPath': 'path/to/appium-uiautomator2-server-test.apk',
+          'appId': 'io.appium.uiautomator2.server.test'
+        }
+      ]).should.eql(false);
+    });
+  });
+
+  describe('canInstallServerPackages', function () {
+    let uiautomator2;
+    const serverApk = {
+      'appPath': 'path/to/appium-uiautomator2-server.apk',
+      'appId': 'io.appium.uiautomator2.server'
+    };
+    const serverTestApk = {
+      'appPath': 'path/to/appium-uiautomator2-server-test.apk',
+      'appId': 'io.appium.uiautomator2.server.test'
+    };
+    beforeEach(function () {
+      uiautomator2 = new UiAutomator2Server(log, {
+        adb,
+        tmpDir: 'tmp',
+        systemPort: 4724,
+        host: 'localhost',
+        devicePort: 6790,
+        disableWindowAnimation: false,
+        disableSuppressAccessibilityService: false
+      });
+    });
+    it('with newer servers are installed', function () {
+      uiautomator2.canInstallServerPackages([
+        {
+          'installState': adb.APP_INSTALL_STATE.NEWER_VERSION_INSTALLED,
+          ...serverApk
+        },
+        {
+          'installState': adb.APP_INSTALL_STATE.NEWER_VERSION_INSTALLED,
+          ...serverTestApk
+        }
+      // since installation may fail
+      ]).should.eql(false);
+    }),
+    it('with newer server is installed but the other could be old one', function () {
+      // Then, enforce to uninstall all apks
+      uiautomator2.canInstallServerPackages([
+        {
+          'installState': adb.APP_INSTALL_STATE.NEWER_VERSION_INSTALLED,
+          ...serverApk
+        },
+        {
+          'installState': adb.APP_INSTALL_STATE.OLDER_VERSION_INSTALLED,
+          ...serverTestApk
+        }
+      ]).should.eql(true);
+    }),
+    it('with newer server is installed', function () {
+      uiautomator2.canInstallServerPackages([
+        {
+          'installState': adb.APP_INSTALL_STATE.SAME_VERSION_INSTALLED,
+          ...serverApk
+        },
+        {
+          'installState': adb.APP_INSTALL_STATE.SAME_VERSION_INSTALLED,
+          ...serverTestApk
+        }
+      ]).should.eql(false);
+    }),
+    it('with older servers are installed', function () {
+      // then, installing newer serves are sufficient.
+      uiautomator2.canInstallServerPackages([
+        {
+          'installState': adb.APP_INSTALL_STATE.OLDER_VERSION_INSTALLED,
+          ...serverApk
+        },
+        {
+          'installState': adb.APP_INSTALL_STATE.OLDER_VERSION_INSTALLED,
+          ...serverTestApk
+        }
+      ]).should.eql(true);
+    }),
+    it('with no server are installed', function () {
+      uiautomator2.canInstallServerPackages([
+        {
+          'installState': adb.APP_INSTALL_STATE.NOT_INSTALLED,
+          'appPath': 'path/to/appium-uiautomator2-server.apk',
+          'appId': 'io.appium.uiautomator2.server'
+        },
+        {
+          'installState': adb.APP_INSTALL_STATE.NOT_INSTALLED,
+          'appPath': 'path/to/appium-uiautomator2-server-test.apk',
+          'appId': 'io.appium.uiautomator2.server.test'
+        }
+      ]).should.eql(true);
+    });
+  });
+
   describe('installServerApk', withMocks({adb, helpers}, (mocks) => {
     let uiautomator2;
     beforeEach(function () {
@@ -35,7 +211,7 @@ describe('UiAutomator2', function () {
 
       // SERVER_PACKAGE_ID
       mocks.adb.expects('getApplicationInstallState').once()
-        .returns(adb.APP_INSTALL_STATE.OLDER_VERSION_INSTALLED);
+        .returns(adb.APP_INSTALL_STATE.NEWER_VERSION_INSTALLED);
 
       // SERVER_PACKAGE_ID and SERVER_TEST_PACKAGE_ID
       mocks.adb.expects('checkApkCert').twice().returns(true);
@@ -58,7 +234,7 @@ describe('UiAutomator2', function () {
 
       // SERVER_PACKAGE_ID
       mocks.adb.expects('getApplicationInstallState').once()
-        .returns(adb.APP_INSTALL_STATE.NEWER_VERSION_INSTALLED);
+        .returns(adb.APP_INSTALL_STATE.OLDER_VERSION_INSTALLED);
 
       // SERVER_PACKAGE_ID and SERVER_TEST_PACKAGE_ID
       mocks.adb.expects('checkApkCert').twice().returns(true);
@@ -146,7 +322,6 @@ describe('UiAutomator2', function () {
         .once().returns(INSTRUMENTATION_TARGET);
       await uiautomator2.installServerApk();
     });
-
 
     it('a server is installed but server.test is not', async function () {
       mocks.helpers.expects('isWriteable').never();
